36b6b7caa6158ad4599101732516ea9c06953c94
Jeff Beeman <jeff@jeffbeeman.com>
2014-09-08 23:02:42 -0400

    Add sample local build

---
+++ scripts/build-local.example.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Change permissions on sites folder to avoid permissions when deleting the folder.
+chmod -R gou+rwx ~/Sites/warp/sites/;
+
+# Remove the existing install.
+rm -Rf ~/Sites/warp;
+
+# Rebuild.
+drush make ~/Projects/drupal/warp/build-warp.make ~/Sites/warp --working-copy --no-gitinfofile;
+
+#
+# Set upstream remotes to which we can push various in-development projects.
+#
+
+# Install profile.
+cd ~/Sites/warp/profiles/warp/;
+git remote add upstream git@github.com:jrbeeman/warp.git;
+
+# Entity index.
+cd ~/Sites/warp/profiles/warp/modules/custom/entity_index/;
+git remote add upstream jrbeeman@git.drupal.org:project/entity_index.git;
+
+# Fast forum.
+cd ~/Sites/warp/profiles/warp/modules/custom/fast_forum/;
+git remote add upstream jrbeeman@git.drupal.org:sandbox/jrbeeman/2073961.git;
+
+# User stats.
+cd ~/Sites/warp/profiles/warp/modules/custom/user_stats/;
+git remote add upstream jrbeeman@git.drupal.org:sandbox/jrbeeman/2314257.git;
+
+#
+# Reinstall the site.
+#
+drush @loc.warp si -yv warp --site-name="Warp (local)" --db-url="mysql://drupaluser:@127.0.0.1:33067/warp";
+
+# Enable developer tools.
+drush @loc.warp dl xhprof;
+drush @loc.warp en xhprof -y;
