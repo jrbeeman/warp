9df82343158bdefda039c899be56f37f337f138c
Jeff Beeman <jeff@jeffbeeman.com>
2014-10-23 20:16:14 -0700

    Enable fcgen example project

--- scripts/build-local.example.sh
+++ scripts/build-local.example.sh
@@ -33,8 +33,3 @@ git remote add upstream jrbeeman@git.drupal.org:sandbox/jrbeeman/2314257.git;
 # Reinstall the site.
 #
 drush @loc.warp si -yv warp --site-name="Warp (local)" --db-url="mysql://drupaluser:@127.0.0.1:33067/warp";
-
-# Enable developer tools.
-# https://www.drupal.org/node/1665688
-drush @loc.warp dl xhprof;
-drush @loc.warp en xhprof -y;
